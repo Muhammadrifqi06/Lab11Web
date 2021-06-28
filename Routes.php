1513e99e1506cbb2ccf95687a9bf31ff4bb0c6ec
Muhammadrifqi06 <81462436+Muhammadrifqi06@users.noreply.github.com>
2021-06-28 22:35:09 +0700

    Add files via upload

--- Routes.php
+++ Routes.php
@@ -33,11 +33,19 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->get('/about', 'Page::about'); 
-$routes->get('/contact', 'Page::contact'); 
-$routes->get('/faqs', 'Page::faqs'); 
-$routes->setAutoRoute(true); 
+$routes->get('/about', 'page::about');
+$routes->get('/contact', 'page::contact');
+$routes->get('/faqs', 'page::faqs');
 
+$routes->get('/artikel/(:any)', 'Artikel::view/$1');
+
+$routes->group('admin', ['filters' => 'auth'], function($routes) 
+{
+	$routes->get('artikel', 'Artikel::admin_index');
+	$routes->add('artikel/add', 'Artikel::add');
+	$routes->add('artikel/edit/(:any)', 'Artikel::edit/$1');
+	$routes->get('artikel/delete/(:any)', 'Artikel::delete/$1');
+});
 /*
  * --------------------------------------------------------------------
  * Additional Routing
